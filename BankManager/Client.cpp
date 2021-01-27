6c0ec50688de85136a6a96280e06c87a64347e31
trajkoArT <trajko97@gmail.com>
2021-01-28 00:18:27 +0100

    Added class definitions and getters

--- BankManager/Client.cpp
+++ BankManager/Client.cpp
@@ -1,14 +1,20 @@
 #include "pch.h"
 #include "Client.h"
 
-Client::Client::Client(std::string Name, std::string Address, std::string DateOfBirth, std::string PinCode)
+
+Client::Client::Client(std::string Name, std::string Address, std::string PhoneNumber, std::string DateOfBirth,
+	 std::string PersonIdentificationNumber, std::string CardNumber, std::string PinCode)
+	: name(Name), address(Address), phoneNumber(PhoneNumber), dateOfBirth(DateOfBirth),
+		personIdentifactionNumber(PersonIdentificationNumber), cardNumber(CardNumber), pinCode(PinCode)
 {
-	this->name = Name;
-	this->address = Address;
-	this->dateOfBirth = DateOfBirth;
-	this->pinCode = PinCode;
+	this->totalAmount = 0.0f;
 }
 
 Client::Client::~Client()
 {
+}
+
+float Client::Client::GetTotalAmount(Account::CurrencyType cType) const
+{
+	return;
 }
